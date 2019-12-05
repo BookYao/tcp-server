501b62e5c1776e99f3f3a6561bf1a4db384e0c04
BookYao <workfile_BookYao@163.com>
2019-12-05 18:11:53 +0800

    1. add msg(set_linkage_watcher) handle

--- msgHandle.go
+++ msgHandle.go
@@ -5,17 +5,21 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
+	"os"
 )
 
 const (
-	AACP_HEAD_LENGHT = 24
+	MSG_HEAD_LENGHT = 24
 )
 
 const (
-	AACP_SET_LINKAGE_WATCHER = 330
+	MSG_PING 				= 1
+	MSG_SET_LINKAGE_WATCHER = 330
 )
 
-type AACPHead struct  {
+
+
+type MSGHead struct  {
 	Length int
 	TermID int
 	CommendID int
@@ -27,29 +31,72 @@ type AACPHead struct  {
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
-	msgHeadBuf := make([]byte, AACP_HEAD_LENGHT)
+	msgHeadBuf := make([]byte, MSG_HEAD_LENGHT)
 	for {
 		length, err := conn.Read(msgHeadBuf)
-		if err != nil  || length != AACP_HEAD_LENGHT {
-			fmt.Println("Read AACP MsgHead Failed.", err.Error())
+		if err != nil  || length != MSG_HEAD_LENGHT {
+			fmt.Println("Read MSG MsgHead Failed.", err.Error())
 			continue
 		}
 
-		aacpHead := AACPHead{}
+		MSGHead := MSGHead{}
 		bytesBuf := bytes.NewBuffer(msgHeadBuf)
-		binary.Read(bytesBuf, binary.LittleEndian, &aacpHead)
-		fmt.Printf("THis AACP Msg Total Length: %d\n", aacpHead.Length)
+		binary.Read(bytesBuf, binary.LittleEndian, &MSGHead)
+		fmt.Printf("THis MSG Msg Total Length: %d\n", MSGHead.Length)
 
-		aacpMsgHandle(conn, &aacpHead)
+		MSGMsgHandle(conn, &MSGHead)
 	}
 }
 
-func aacpMsgHandle(conn net.Conn, aacpHead *AACPHead)  {
-	msgBodyLen := aacpHead.Length - AACP_HEAD_LENGHT
-	switch aacpHead.Type {
-	case AACP_SET_LINKAGE_WATCHER:
-		fmt.Println("Recv Set Linkage Watcher MSG, bodylen:", msgBodyLen)
-	default:
-		fmt.Println("Unknown AACP Msg type.")
+func saveLinkageWatcher(msg []byte) bool {
+	LINKAGE_WATCHER_FILE  := "/var/tmp/linkage.conf"
+	fp, err := os.Open(LINKAGE_WATCHER_FILE)
+	if err != nil {
+		fmt.Println("Open Linkage Watcher File Failed.", err)
+		return false
 	}
+
+	_, err = fp.Write(msg)
+	if err != nil {
+		fp.Close()
+		fmt.Println("Write Linkage Watcher File Failed.", err)
+		return false
+	}
+	fp.Close()
+	return true
+}
+
+func MSGMsgHandle(conn net.Conn, MSGHead *MSGHead) bool {
+	var status bool =  true
+	msgBodyLen := MSGHead.Length - MSG_HEAD_LENGHT
+	switch MSGHead.Type {
+	case MSG_PING:
+		buf := make([]byte, msgBodyLen)
+		len, err := conn.Read(buf)
+		if err != nil || len == 0 {
+			fmt.Println("Set Linkage Watcher Read failed.", err)
+			status = false
+		}
+		_, err = conn.Write(buf)
+		if err != nil {
+			fmt.Println("MSG Ping Write Failed.", err)
+			status = false
+		}
+	case MSG_SET_LINKAGE_WATCHER:
+		buf := make([]byte, msgBodyLen)
+		len, err := conn.Read(buf)
+		if err != nil  || len == 0 {
+			fmt.Println("Set Linkage Watcher Read failed.", err)
+			status = false
+		}
+		fmt.Printf("Recv Set Linkage Watcher MSG, bodylen:%d-recvlen:%d\n", msgBodyLen, len)
+		if saveLinkageWatcher(buf) != true {
+			fmt.Println("Save Linkage Watcher Failed.")
+			status = false
+		}
+	default:
+		fmt.Println("Unknown MSG Msg type.")
+	}
+
+	return status
 }
